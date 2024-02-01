ef2f6cbed4f6cb713eb102d82cab9d14677cd0f1
felipe-jaque-dev <felipealexandrej@gmail.com>
2024-01-31 22:26:12 -0300

    fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostage
 function Home() {
     return (
         <>
-        <div className="bg-indigo-900 flex justify-center">
+        <div className="bg-indigo-900 flex justify-center .imagem_fundo">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
